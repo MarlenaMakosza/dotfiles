58e71ebff668053ff83061f92ab82a81155d4fc8
Marlena Mąkosza <marlenamakosza@gmail.com>
2026-06-21 18:34:44 +0200

    Update .zshrc.d/55-functions.zsh

--- dot_zshrc.d/55-functions.zsh
+++ dot_zshrc.d/55-functions.zsh
@@ -2,5 +2,6 @@
 mdtopdf() {
   pandoc "$1" -o "${1:r}.pdf" \
     --pdf-engine=xelatex \
-    -V geometry:margin=2.5cm
+    -V geometry:margin=2.5cm \
+    -H <(printf '\\usepackage{titlesec}\n\\newcommand\\sectionbreak{\\clearpage}')
 }
